46357f2504790edddfaf6ea1f83de8edd46a0d91
ymkim0216 <qkslzl101216@naver.com>
2023-02-16 12:15:54 +0900

    레벨0 로그인성공?

---
+++ src/level0/login.java
@@ -0,0 +1,21 @@
+package level0;
+
+public class login {
+
+	public String solution(String[] id_pw, String[][] db) {
+        String answer = "";
+        for(int i=0;i<db.length;i++) {
+        	if(id_pw[0].equals(db[i][0])&&id_pw[1].equals(db[i][1])) {
+        		answer = "login";
+        		break;
+        	}else if(id_pw[0].equals(db[i][0])&&!id_pw[1].equals(db[i][1])) {
+        		answer = "wrong pw";
+        		break;
+        	}else {
+        		answer = "fail";
+        	}
+        }
+        return answer;
+    }
+	
+}
